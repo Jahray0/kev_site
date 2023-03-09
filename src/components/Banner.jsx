115182952de0b0d08eb06a5e0d3d1522c25d5c1e
Jahray0 <jaegle.maxime@gmail.com>
2023-03-09 09:56:02 +0100

    banner responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,11 +4,9 @@ import banner from "../assets/banner.jpg";
 function Banner({ titre }) {
   return (
     <div className="container">
+      <img src={banner} alt="Bannière à propos" />
       <div className="title_banner">
         <h1>{titre}</h1>
-      </div>
-      <div className="img_content">
-        <img src={banner} alt="Bannière à propos" />
       </div>
     </div>
   );
