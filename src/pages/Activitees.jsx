86f11b10f8d7bb30b88a5b0f07479f5e5d48216d
Jahray0 <jaegle.maxime@gmail.com>
2023-03-10 11:57:44 +0100

    test une animation sur card

--- src/pages/Activitees.jsx
+++ src/pages/Activitees.jsx
@@ -1,85 +1,57 @@
+import "./Activitees.css";
 import "../components/Banner.css";
 import Banner from "../components/Banner";
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
+import image4 from "../assets/image4.jpg";
 
 function Activitees() {
   return (
-    <div className="content">
+    <div className="content_p2">
       <Banner titre="Activitées" />
-      <h1>Bonjour a tous</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac magna
-        nisi. Duis rutrum consectetur lorem id auctor. Mauris mollis vestibulum
-        laoreet. Vestibulum auctor tortor quis eros maximus, nec ullamcorper
-        velit tempor. Donec et libero in mi interdum posuere. Ut id orci lacus.
-        Pellentesque tempor elit lorem, nec ultricies lorem finibus sit amet.
-        Mauris sagittis, velit a pulvinar egestas, felis metus viverra massa, eu
-        cursus nisi dolor porttitor diam. Morbi quis sem auctor, finibus augue
-        ullamcorper, cursus massa. Nullam dictum volutpat ligula sit amet porta.
-        Donec consequat purus lacus, a accumsan sapien rhoncus id. Vestibulum
-        porttitor maximus lectus, sed tristique mauris efficitur et. Mauris
-        cursus massa aliquet, porta purus sit amet, lacinia nulla. Maecenas id
-        placerat arcu. Donec imperdiet, mi ut placerat convallis, libero libero
-        dictum lacus, vitae suscipit risus elit a elit. Aenean in egestas est.
-        Ut pretium interdum mi vel maximus. Quisque quis vestibulum turpis,
-        vitae suscipit risus. Etiam porta auctor velit sit amet ultricies.
-        Praesent nibh nunc, porta et cursus vel, tincidunt in elit. Proin vitae
-        consequat purus. Sed sed euismod ipsum, auctor dignissim elit. Etiam at
-        dignissim sapien. Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Duis ac magna nisi. Duis rutrum consectetur lorem id auctor.
-        Mauris mollis vestibulum laoreet. Vestibulum auctor tortor quis eros
-        maximus, nec ullamcorper velit tempor. Donec et libero in mi interdum
-        posuere. Ut id orci lacus. Pellentesque tempor elit lorem, nec ultricies
-        lorem finibus sit amet. Mauris sagittis, velit a pulvinar egestas, felis
-        metus viverra massa, eu cursus nisi dolor porttitor diam. Morbi quis sem
-        auctor, finibus augue ullamcorper, cursus massa. Nullam dictum volutpat
-        ligula sit amet porta. Donec consequat purus lacus, a accumsan sapien
-        rhoncus id. Vestibulum porttitor maximus lectus, sed tristique mauris
-        efficitur et. Mauris cursus massa aliquet, porta purus sit amet, lacinia
-        nulla. Maecenas id placerat arcu. Donec imperdiet, mi ut placerat
-        convallis, libero libero dictum lacus, vitae suscipit risus elit a elit.
-        Aenean in egestas est. Ut pretium interdum mi vel maximus. Quisque quis
-        vestibulum turpis, vitae suscipit risus. Etiam porta auctor velit sit
-        amet ultricies. Praesent nibh nunc, porta et cursus vel, tincidunt in
-        elit. Proin vitae consequat purus. Sed sed euismod ipsum, auctor
-        dignissim elit. Etiam at dignissim sapien. Nullam aliquet magna eget
-        nibh suscipit, in tristique diam faucibus. Integer rutrum id tortor
-        aliquet tempus. Aliquam pulvinar, ante sit amet aliquam euismod, nisi
-        felis luctus nisi, dapibus iaculis augue diam id risus. Maecenas
-        hendrerit vulputate accumsan. Ut fermentum sed elit eget suscipit.
-        Pellentesque pellentesque at libero ut consequat. Etiam vestibulum odio
-        in ligula dictum, ut placerat nibh interdum. Donec finibus odio ac
-        molestie mollis. Suspendisse a tellus sed eros lobortis dapibus feugiat
-        vel tortor. In fringilla ullamcorper velit, sit amet consectetur mi
-        accumsan ac. Ut diam lorem, interdum in ipsum eu, venenatis varius
-        justo. Pellentesque blandit pellentesque lorem, sed sagittis nibh tempus
-        et. Donec suscipit lacus magna, ac pulvinar magna viverra at. Proin
-        efficitur nunc ac nunc pulvinar pharetra. Sed ullamcorper tortor sed
-        elit feugiat tincidunt. Maecenas mattis quam ac dolor pharetra, blandit
-        gravida justo ornare. Nunc egestas odio eget suscipit elementum. Sed
-        quis enim urna. Vestibulum ante ipsum primis in faucibus orci luctus et
-        ultrices posuere cubilia curae; Aenean bibendum lacus et tellus placerat
-        tincidunt. Ut felis nulla, porta laoreet lectus a, venenatis dapibus
-        risus. Nunc tempus dui viverra nunc faucibus blandit. Praesent interdum
-        ante vitae augue malesuada, ut sollicitudin metus semper. Duis mi eros,
-        feugiat et leo eget, accumsan egestas orci. Nam sagittis quam vel ipsum
-        fringilla, non tincidunt est maximus. Suspendisse dictum tincidunt
-        volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Cras non facilisis erat, vitae fringilla urna. Integer nec posuere
-        risus. Sed eros leo, laoreet in nisi sed, pellentesque mollis eros. Nam
-        porta interdum laoreet. Proin et nisi suscipit, mollis eros sed, feugiat
-        erat. Cras velit justo, aliquam id elit in, ultricies mattis libero.
-        Praesent luctus consequat magna sed imperdiet. Proin rutrum quis ex id
-        aliquam. Pellentesque consequat volutpat odio. Nunc elit libero, mollis
-        vel dui id, sagittis ultricies leo. Nam aliquet sapien sit amet purus
-        elementum sollicitudin. Vivamus ut odio ac neque suscipit vestibulum.
-        Praesent rhoncus mi et ex hendrerit, id consectetur dolor egestas. Donec
-        convallis tortor nec metus laoreet, a aliquet est dapibus. Sed pharetra
-        lacinia eros in molestie. In id felis dictum, tristique nulla sit amet,
-        efficitur quam. Aliquam auctor facilisis turpis eu pulvinar. Nunc est
-        velit, scelerisque quis arcu nec, finibus fermentum metus. Nullam
-        fringilla eros ac dui interdum luctus. Cras id dolor at sapien sodales
-        pharetra et quis nisi. Cras elementum elementum nibh.
-      </p>
+      <div className="act_content">
+        <div className="act_title">
+          <h2>Activitées</h2>
+        </div>
+        <div className="act_vignettes">
+          <div className="cards">
+            <div className="card_title">
+              <h3>Randonnées</h3>
+            </div>
+            <div className="card_image">
+              <img src={image1} alt="Bannière à propos" />
+            </div>
+          </div>
+
+          <div className="cards">
+            <div className="card_title">
+              <h3>Raquettes</h3>
+            </div>
+            <div className="card_image">
+              <img src={image2} alt="Bannière à propos" />
+            </div>
+          </div>
+
+          <div className="cards">
+            <div className="card_title">
+              <h3>Faune et Flore</h3>
+            </div>
+            <div className="card_image">
+              <img src={image3} alt="Bannière à propos" />
+            </div>
+          </div>
+
+          <div className="cards">
+            <div className="card_title">
+              <h3>Orientation</h3>
+            </div>
+            <div className="card_image">
+              <img src={image4} alt="Bannière à propos" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
